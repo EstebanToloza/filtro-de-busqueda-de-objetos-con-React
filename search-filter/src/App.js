f1e90cfc401498153dc74ac0c06e159217efee0c
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-05-22 16:04:48 -0300

    Filtro por nombre, ok

--- search-filter/src/App.js
+++ search-filter/src/App.js
@@ -2,179 +2,181 @@ import { useState } from "react";
 
 function App() {
 
-  const dataList = [
+  const productsList = [
     {
       "id": 1,
       "name": "cerulean",
-      "year": 2000,
+      "model": 2000,
       "color": "#98B2D1",
-      "pantone_value": "15-4020"
+      "stock": "15-4020"
     },
     {
       "id": 2,
       "name": "fuchsia rose",
-      "year": 2001,
+      "model": 2001,
       "color": "#C74375",
-      "pantone_value": "17-2031"
+      "stock": "17-2031"
     },
     {
       "id": 3,
       "name": "true red",
-      "year": 2002,
+      "model": 2002,
       "color": "#BF1932",
-      "pantone_value": "19-1664"
+      "stock": "19-1664"
     },
     {
       "id": 4,
       "name": "aqua sky",
-      "year": 2003,
+      "model": 2003,
       "color": "#7BC4C4",
-      "pantone_value": "14-4811"
+      "stock": "14-4811"
     },
     {
       "id": 5,
       "name": "tigerlily",
-      "year": 2004,
+      "model": 2004,
       "color": "#E2583E",
-      "pantone_value": "17-1456"
+      "stock": "17-1456"
     },
     {
       "id": 6,
       "name": "blue turquoise",
-      "year": 2005,
+      "model": 2005,
       "color": "#53B0AE",
-      "pantone_value": "15-5217"
+      "stock": "15-5217"
     },
     {
       "id": 7,
       "name": "sand dollar",
-      "year": 2006,
+      "model": 2006,
       "color": "#DECDBE",
-      "pantone_value": "13-1106"
+      "stock": "13-1106"
     },
     {
       "id": 8,
       "name": "chili pepper",
-      "year": 2007,
+      "model": 2007,
       "color": "#9B1B30",
-      "pantone_value": "19-1557"
+      "stock": "19-1557"
     },
     {
       "id": 9,
       "name": "blue iris",
-      "year": 2008,
+      "model": 2008,
       "color": "#5A5B9F",
-      "pantone_value": "18-3943"
+      "stock": "18-3943"
     },
     {
       "id": 10,
       "name": "mimosa",
-      "year": 2009,
+      "model": 2009,
       "color": "#F0C05A",
-      "pantone_value": "14-0848"
+      "stock": "14-0848"
     },
     {
       "id": 11,
       "name": "turquoise",
-      "year": 2010,
+      "model": 2010,
       "color": "#45B5AA",
-      "pantone_value": "15-5519"
+      "stock": "15-5519"
     },
     {
       "id": 12,
       "name": "honeysuckle",
-      "year": 2011,
+      "model": 2011,
       "color": "#D94F70",
-      "pantone_value": "18-2120"
+      "stock": "18-2120"
     },
     {
       "id": 13,
       "name": "cerulean",
-      "year": 2000,
+      "model": 2000,
       "color": "#98B2D1",
-      "pantone_value": "15-4020"
+      "stock": "15-4020"
     },
     {
       "id": 14,
       "name": "fuchsia rose",
-      "year": 2001,
+      "model": 2001,
       "color": "#C74375",
-      "pantone_value": "17-2031"
+      "stock": "17-2031"
     },
     {
       "id": 15,
       "name": "true red",
-      "year": 2002,
+      "model": 2002,
       "color": "#BF1932",
-      "pantone_value": "19-1664"
+      "stock": "19-1664"
     },
     {
       "id": 16,
       "name": "aqua sky",
-      "year": 2003,
+      "model": 2003,
       "color": "#7BC4C4",
-      "pantone_value": "14-4811"
+      "stock": "14-4811"
     },
     {
       "id": 17,
       "name": "tigerlily",
-      "year": 2004,
+      "model": 2004,
       "color": "#E2583E",
-      "pantone_value": "17-1456"
+      "stock": "17-1456"
     },
     {
       "id": 18,
       "name": "blue turquoise",
-      "year": 2005,
+      "model": 2005,
       "color": "#53B0AE",
-      "pantone_value": "15-5217"
+      "stock": "15-5217"
     },
     {
       "id": 19,
       "name": "sand dollar",
-      "year": 2006,
+      "model": 2006,
       "color": "#DECDBE",
-      "pantone_value": "13-1106"
+      "stock": "13-1106"
     },
     {
       "id": 20,
       "name": "chili pepper",
-      "year": 2007,
+      "model": 2007,
       "color": "#9B1B30",
-      "pantone_value": "19-1557"
+      "stock": "19-1557"
     },
     {
       "id": 21,
       "name": "blue iris",
-      "year": 2008,
+      "model": 2008,
       "color": "#5A5B9F",
-      "pantone_value": "18-3943"
+      "stock": "18-3943"
     },
     {
       "id": 22,
       "name": "mimosa",
-      "year": 2009,
+      "model": 2009,
       "color": "#F0C05A",
-      "pantone_value": "14-0848"
+      "stock": "14-0848"
     },
     {
       "id": 23,
       "name": "turquoise",
-      "year": 2010,
+      "model": 2010,
       "color": "#45B5AA",
-      "pantone_value": "15-5519"
+      "stock": "15-5519"
     },
     {
       "id": 24,
       "name": "honeysuckle",
-      "year": 2011,
+      "model": 2011,
       "color": "#D94F70",
-      "pantone_value": "18-2120"
+      "stock": "18-2120"
     }
   ];
 
-  const [searchText, setSearchText] = useState('');
-  const [data, setData] = useState(dataList)
+  const [searchText, setSearchText] = useState();
+  const [data, setData] = useState(productsList)
+
+  const excludeProperties = ['id', 'model', 'color', 'stock']
 
   const handleChange = value => {
     setSearchText(value);
@@ -184,12 +186,12 @@ function App() {
   const filterData = value => {
     const lowerCaseValue = value.toLowerCase().trim();
     if(!lowerCaseValue) {
-      setData(dataList);
+      setData(productsList);
     } 
     else {
-      const filteredData = dataList.filter(item => {
+      const filteredData = productsList.filter(item => {
         return Object.keys(item).some(key => {
-          return item[key].toString().toLowerCase().includes(lowerCaseValue);
+          return excludeProperties.includes(key) ? false : item[key].toString().toLowerCase().includes(lowerCaseValue);
         })
       });
       setData(filteredData);
@@ -210,9 +212,9 @@ function App() {
           return (
             <div className="box" key={i} style={{ backgroundColor: item.color }}>
               <b>Name: </b>{item.name}<br/>
-              <b>Year: </b>{item.year}<br/>
+              <b>Model: </b>{item.model}<br/>
               <b>Color: </b>{item.color}<br/>
-              <b>Pantone value: </b>{item.pantone_value}<br/>
+              <b>Stock: </b>{item.stock}<br/>
             </div>)
         })}
         <div className="clearBoth"></div>
